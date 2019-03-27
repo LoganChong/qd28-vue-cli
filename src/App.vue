82460539d9787a34379bb0957c381ebfa21957f1
logan <1215714989@qq.com>
2019-03-27 16:40:04 +0800

    完成了elementUI和axios模块的引入

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     hello vue-cli
 
+    <!-- 调用Element UI组件 -->
+    <el-row>
+      <el-button type="primary">主要按钮</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-
+  name: "app",
+  components: {},
+  mounted:function (){
+    this.$axios.get("https://api.github.com/users").then(res => {
+      console.log(res.data)
+    })
   }
-}
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 非生产环境
 Vue.config.productionTip = false
+
+// 1.引入element-ui
+import ElementUI from "element-ui";
+// 2.引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 1.引入axios
+import axios from 'axios';
+
+// 3.全局注册组件
+Vue.use(ElementUI);
+
+// 2.把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
+
+
+// --------------------------------------------
+// 之前的根实例
+// new Vue({
+//   el: "#app",
+
+//   render: function(createElement){
+//     return createElement(App)
+//   },
+
+//   // 箭头函数只有一行代码并且需要返回的时候
+//   render: h = h(App)
+// })
